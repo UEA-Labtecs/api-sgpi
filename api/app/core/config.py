6cee72e476d76d4df091a27c88e30a9c707a47f9
Thiago Soares <devthiagosoares@gmail.com>
2025-07-29 17:18:43 -0300

    a

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -10,9 +10,9 @@ ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1440"))  # default 1 dia
 
 # Conexão com o banco
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "0.0.0.0")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
